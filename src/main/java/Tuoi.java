b22f86f24740cf6cd8c0ae6a003fe5409a26a96a
Thaodoan1234 <thao.doan1@onemount.com>
2021-08-06 01:34:21 +0700

    Initial Commit

---
+++ src/main/java/Tuoi.java
@@ -0,0 +1,9 @@
+public class Tuoi {
+    public static void main(String[] args) {
+        int namsinh = 1980;
+        int tuoi = 2021 - namsinh;
+        System.out.println("Năm hiện tại: 2021");
+        System.out.println("Năm sinh: " + namsinh);
+        System.out.println("Tuoi: " + tuoi);
+    }
+}
